028e556bc2665f035b9a45af01293b87ab0ec6bf
Satish Pasala <spasala@logixhealth.com>
2024-10-24 16:54:12 -0400

    Added Product Repository.

---
+++ gRPC.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using gRPC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRPC.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductEntity>> GetAllAsync();
+
+        Task<ProductEntity?> GetProductByIdAsync(int productId);
+
+        Task<int> CreateProductAsync(ProductEntity product);
+
+        Task<int> UpdateProductAsync(ProductEntity product);
+
+        Task<int> DeleteProductAsync(ProductEntity product); 
+    }
+}
---
+++ gRPC.Infrastructure/Persistance/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using gRPC.Domain.Entities;
+using gRPC.Domain.Repositories;
+using gRPC.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRPC.Infrastructure.Persistance
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public ProductRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<ProductEntity>> GetAllAsync()
+        {
+            return await _appDbContext.Products.ToListAsync();
+        }
+
+        public async Task<ProductEntity?> GetProductByIdAsync(int productId)
+        {
+            return await _appDbContext.Products.FirstOrDefaultAsync(p => p.Id == productId);
+        }
+
+        public async Task<int> CreateProductAsync(ProductEntity product)
+        {
+            await _appDbContext.Products.AddAsync(product);
+            await _appDbContext.SaveChangesAsync();
+            return product.Id;
+        }
+
+        public async Task<int> UpdateProductAsync(ProductEntity product)
+        {
+            _appDbContext.Products.Update(product);
+            await _appDbContext.SaveChangesAsync();
+            return product.Id;
+        }
+
+        public async Task<int> DeleteProductAsync(ProductEntity product)
+        {
+            _appDbContext.Remove(product);
+            await _appDbContext.SaveChangesAsync();
+            return product.Id;
+        }
+
+    }
+}
