b000552181be793971dd49e4e020b1104d9d7840
Satish Pasala <spasala@logixhealth.com>
2024-10-24 15:10:06 -0400

    Added projects, folders, and code clean up. Added Product Service.

---
+++ gRPC.Service/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using gRPC.Infrastructure.Protos.product;
+
+
+namespace gRPC.Service.Services;
+
+public class ProductService : Product.ProductBase
+{ 
+    public ProductService() { }
+
+    public override async Task<ProductResponse> ReadProduct(ProductRequest request, ServerCallContext context)
+    {
+        if (request == null || request.Id <= 0 ) throw new RpcException(new Status(StatusCode.InvalidArgument, "Enter a valid request product data."));
+
+        return await Task.FromResult(new ProductResponse
+        {
+            Id = 1,
+            ProductName = "Fan"
+        });
+    }
+}
