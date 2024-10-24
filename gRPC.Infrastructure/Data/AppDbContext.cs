8ca756ce8a90549ee3dcc7bfb99c2351aa8e91ea
Satish Pasala <spasala@logixhealth.com>
2024-10-24 15:55:03 -0400

    Added DbContext.

--- gRPC.Infrastructure/Data/AppDbContext.cs
+++ gRPC.Infrastructure/Data/AppDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using gRPC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace gRPC.Infrastructure.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ProductEntity> Products => Set<ProductEntity>();
     }
 }
--- gRPC.Service/Program.cs
+++ gRPC.Service/Program.cs
@@ -1,13 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using gRPC.Infrastructure.Data;
 using gRPC.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=Shopping.db"));
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<ProductService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
