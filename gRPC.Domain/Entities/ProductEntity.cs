eff790ceee34fbaef0a14692d808bfd30c0a1874
Satish Pasala <spasala@logixhealth.com>
2024-10-24 15:54:35 -0400

    Added product entity.

--- gRPC.Domain/Entities/ProductEntity.cs
+++ gRPC.Domain/Entities/ProductEntity.cs
@@ -9,5 +9,17 @@ namespace gRPC.Domain.Entities
     public class ProductEntity
     {
         public int Id { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public string ProductDescription { get; set; } = string.Empty;
+
+        public string ProductType { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public string Vendor { get; set; } = string.Empty;
     }
 }
