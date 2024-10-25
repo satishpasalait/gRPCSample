cfbc3d3f8527445a534480cd0b4445da14ae6544
Satish Pasala <spasala@logixhealth.com>
2024-10-25 16:29:46 -0400

    Added Read operation.

--- gRPC.Service/Program.cs
+++ gRPC.Service/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using gRPC.Infrastructure.Data;
 using gRPC.Service.Services;
+using gRPC.Domain.Repositories;
+using gRPC.Infrastructure.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=Database/Shopping.db"));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddGrpc();
 
--- gRPC.Service/Services/ProductService.cs
+++ gRPC.Service/Services/ProductService.cs
@@ -1,21 +1,34 @@
 ﻿using Grpc.Core;
 using gRPC.Infrastructure.Protos.product;
+using gRPC.Domain.Repositories;
 
 
 namespace gRPC.Service.Services;
 
 public class ProductService : Product.ProductBase
 { 
-    public ProductService() { }
+    private readonly IProductRepository _productRepository;
+    public ProductService(IProductRepository productRepository) 
+    { 
+        _productRepository = productRepository;
+    }
 
     public override async Task<ProductResponse> ReadProduct(ProductRequest request, ServerCallContext context)
     {
         if (request == null || request.Id <= 0 ) throw new RpcException(new Status(StatusCode.InvalidArgument, "Enter a valid request product data."));
 
+        var product = await _productRepository.GetProductByIdAsync(request.Id) 
+            ?? throw new RpcException(new Status(StatusCode.NotFound, "Product was not found."));
+
         return await Task.FromResult(new ProductResponse
         {
-            Id = 1,
-            ProductName = "Fan"
+            Id = product.Id,
+            ProductName = product.ProductName,
+            ProductDescription = product.ProductDescription,
+            ProductType = product.ProductType,
+            Quantity = product.Quantity,
+            UnitPrice = Convert.ToSingle(product.UnitPrice),
+            Vendor = product.Vendor,
         });
     }
 }
