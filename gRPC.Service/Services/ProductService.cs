54bdbbdc38d9bc835aa019dd15034f536cda2b51
Satish Pasala <spasala@logixhealth.com>
2024-10-28 14:49:50 -0400

    Added Create service.

--- gRPC.Service/Services/ProductService.cs
+++ gRPC.Service/Services/ProductService.cs
@@ -2,6 +2,7 @@
 using gRPC.Infrastructure.Protos.product;
 using gRPC.Domain.Repositories;
 using AutoMapper;
+using gRPC.Domain.Entities;
 
 
 namespace gRPC.Service.Services;
@@ -18,7 +19,7 @@ public class ProductService : Product.ProductBase
 
     public override async Task<ReadProductResponse> ReadProduct(ReadProductRequest request, ServerCallContext context)
     {
-        if (request == null || request.Id <= 0 ) throw new RpcException(new Status(StatusCode.InvalidArgument, "Enter a valid request product data."));
+        if (request == null || request.Id <= 0 ) throw new RpcException(new Status(StatusCode.InvalidArgument, "Enter valid product data."));
 
         var product = await _productRepository.GetProductByIdAsync(request.Id) 
             ?? throw new RpcException(new Status(StatusCode.NotFound, "Product was not found."));
@@ -28,6 +29,20 @@ public class ProductService : Product.ProductBase
         return await Task.FromResult(new ReadProductResponse
         {
             Product = responseProduct.Product
+        });
+    }
+
+    public override async Task<CreateProductResponse> CreateProduct(CreateProductRequest request, ServerCallContext context)
+    {
+        if (request.Product == null) throw new RpcException(new Status(StatusCode.InvalidArgument, "Enter valid product data."));
+
+        var createProduct = _mapper.Map<ProductEntity>(request.Product);
+
+        var result = await _productRepository.CreateProductAsync(createProduct);
+
+        return await Task.FromResult(new CreateProductResponse
+        { 
+            Id = result
         });
     }
 }
